76dc3ddd83120f91c33eb2862bbd0ae15df3ea14
Phemaries <femiodunjo@gmail.com>
2023-11-29 11:35:20 +0100

    Handle Possible Errors

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import Link from 'next/link'
 
 const WebSearchPage = async ( {searchParams}: any ) => {
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_API_KEY}&q=${searchParams.searchTerm}`)
   const res = await response.json()
 
+  if(!response.ok) {
+    throw new Error("Something went wrong!")
+  }
   const results = res.items
+  console.log(results)
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to the homepage{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
 
   return (
     <>
