dcb547674aa6f7fb8a5907a06fcf3b9195d54453
Phemaries <femiodunjo@gmail.com>
2023-11-29 11:01:20 +0100

    fetch data from Google API and display titles

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const WebSearchPage = () => {
+const WebSearchPage = async ( {searchParams}: any ) => {
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_API_KEY}&q=${searchParams.searchTerm}`)
+  const res = await response.json()
+
+  const results = res.items
+
   return (
-    <div>WebSearchPage</div>
+    <>
+      {results && results.map((result: any) => <h1>{result.title}</h1>)}
+    </>
   )
 }
 
